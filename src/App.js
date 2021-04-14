0ccd913ac61fbbf527b428cc31f5ef91cd14a409
Seth Klusmire <sethklusmire@gmail.com>
2021-04-13 20:45:10 -0600

    start of react portfolio. Adding components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import About from "./Components/About/about"
+import Home from "./Components/Home/home"
+import NavBar from "./Components/NavBar/navbar"
+import Projects from "./Components/Projects/projects"
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
