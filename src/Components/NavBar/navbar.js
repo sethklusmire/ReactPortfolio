b9f062c6caadc0bd6b113512e2bda57fe7878d56
Seth Klusmire <sethklusmire@gmail.com>
2021-04-22 16:32:10 -0600

    navbar is better

--- src/Components/NavBar/navbar.js
+++ src/Components/NavBar/navbar.js
@@ -1,54 +1,72 @@
-import React, { useState } from 'react'
-import "./style.css";
-import { Menu } from 'semantic-ui-react'
+import React from 'react'
+import {Link} from 'react-router-dom'
+import classNames from 'classnames'
+import './navbar.css'
 
-function NavBar() {
-  const [name, setName] = useState({})
-
-//   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  const handleInputClick = (e) => {
-      setName(e.target.value)
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showMobileMenu: false
+    }
+    this.toggleMobileMenu = this.toggleMobileMenu.bind(this)
+    this.closeMobileMenu = this.closeMobileMenu.bind(this)
   }
 
+  buildLink(pathName, linkText) {
+    let homeClasses = classNames('nav-item', {active: this.props.location.pathname === pathName})
+    return <Link to={pathName} className={homeClasses}><li>{linkText}</li></Link>
+  }
+
+  buildMobileLink(pathName, linkText) {
+    let homeClasses = classNames('mobile-item', {active: this.props.location.pathname === pathName})
+    return <Link to={pathName} className={homeClasses} onClick={this.closeMobileMenu}><li>{linkText}</li></Link>
+  }
+
+  toggleMobileMenu() {
+    this.setState({
+      showMobileMenu: !this.state.showMobileMenu
+    })
+  }
+
+  closeMobileMenu() {
+    this.setState({
+      showMobileMenu: false
+    })
+  }
+
+  render() {
     return (
-      <Menu>
-        <Menu.Item
-          name='home'
-        //   active={activeItem === 'home'}
-          onClick={handleInputClick}
-        >
-          Home
-        </Menu.Item>
-
-        <Menu.Item
-          name='about'
-        //   active={activeItem === 'projects'}
-          onClick={handleInputClick}
-        >
-          About
-        </Menu.Item>
-
-        <Menu.Menu position='right'>
-          <Menu.Item
-            name='projects'
-            // active={activeItem === 'resume'}
-            onClick={handleInputClick}
-          >
-            Projects
-          </Menu.Item>
-
-          <Menu.Item
-            name='contact'
-            // active={activeItem === 'contact'}
-            onClick={handleInputClick}
-          >
-            Contact
-          </Menu.Item>
-        </Menu.Menu>
-      </Menu>
+      <header className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='nav-logo'>
+            <h2 className='nav-logo-text'>sk</h2>
+          </Link>
+          <nav className='nav-container'>
+            <ul className='right-nav-items'>
+              {this.buildLink('/', 'Home')}
+              {this.buildLink('/projects', 'Projects')}
+              {this.buildLink('/about', 'About')}
+              {this.buildLink('/contact', 'Contact')}
+            </ul>
+            <div className={this.state.showMobileMenu ? 'hamburger-container change' : 'hamburger-container'} onClick={this.toggleMobileMenu}>
+              <div className="bar1"></div>
+              <div className="bar2"></div>
+              <div className="bar3"></div>
+            </div>
+          </nav>
+        </div>
+        <div className={this.state.showMobileMenu ? 'mobile-menu-display' : 'mobile-menu-hidden'} >
+          <ul className='mobile-items'>
+            {this.buildMobileLink('/', 'Home')}
+            {this.buildMobileLink('/projects', 'Projects')}
+            {this.buildMobileLink('/about', 'About')}
+            {this.buildMobileLink('/contact', 'Contact')}
+          </ul>
+        </div>
+      </header>
     )
-  
+  }
 }
 
 export default NavBar
